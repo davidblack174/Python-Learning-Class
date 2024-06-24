792420221fcb4ff733b65436fc1729a3717bb970
David Odediran <davidblack174@gmail.com>
2024-06-24 13:50:04 +0000

    I add two numbers together to proce a result

--- Introduction.py
+++ Introduction.py
@@ -1,2 +1,10 @@
 for i in range(0,5):
     print("Enter the number: ", i)
+
+#Calculate the addition of two numbers
+a = 10
+b = 20
+c = a + b
+#Print the addition of two numbers
+print("The addition of two numbers is: ", c)
+
