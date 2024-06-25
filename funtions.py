938f7f750ce0600b4d606b8c3095be6cf5d6af28
David Odediran <davidblack174@gmail.com>
2024-06-25 11:50:06 +0000

    Class Updates on function

---
+++ funtions.py
@@ -0,0 +1,18 @@
+#A returned function that returns a value
+def add(x):
+     y= x+1
+     return y
+
+print(add(5))
+
+
+#Non Returning function
+a=1
+b=2
+c=3
+
+def add():
+    d=a+b+c
+    print(d)
+
+add() 
