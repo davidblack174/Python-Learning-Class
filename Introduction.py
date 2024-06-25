c73b8a20a4ed292483e80ab17c698cef8d243b41
David Odediran <davidblack174@gmail.com>
2024-06-25 10:47:50 +0000

    Completed the basic yesterday 24-july-2024

--- Introduction.py
+++ Introduction.py
@@ -1,10 +1,24 @@
-for i in range(0,5):
-    print("Enter the number: ", i)
+try:
+    for i in range(0,5):
+         var=[i]
+         print("Enter the number: ", i)
+         print("The number is: ", var)
+except:
+    print("Error Occured")
 
+finally:
+    print("End of the program")
+
+
+
+
+
+    
 #Calculate the addition of two numbers
 a = 10
 b = 20
 c = a + b
 #Print the addition of two numbers
 print("The addition of two numbers is: ", c)
+
 
